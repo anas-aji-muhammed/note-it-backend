47d82634950847540a43268ec05e6a4b6cee27cb
anasajimuhammed <anas.ajeeb2@gmail.com>
2024-01-10 16:14:27 +0000

    first commit

---
+++ src/main/java/com/anasajimuhammed/noteitbackend/NoteItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.noteitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteItBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anasajimuhammed/noteitbackend/NoteItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.noteitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
